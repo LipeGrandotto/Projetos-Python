11c3c5d8371532149dbdbcd507726102c7ac36fa
Luiz Fellipe Grandotto <lfellipegrandottoviana@gmail.com>
2025-02-15 12:12:35 -0300

    Delete Ex48.py

--- Ex48.py
+++
@@ -1,9 +0,0 @@
-# EXERCICIO 48
-
-soma = 0
-cont = 0
-for c in range(1,501, 2):
-   if c % 3 == 0:
-       cont = cont + 1 #cont += 1
-       soma = soma + c #soma += c
-print('A soma de todos os {} valores solicitados é {}'.format(cont, soma))
