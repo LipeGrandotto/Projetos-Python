8e15904e63f4f606a04a62bf3a6694fe30887dfa
Luiz Fellipe Grandotto <lfellipegrandottoviana@gmail.com>
2025-02-15 12:13:59 -0300

    Delete Ex57.py

--- Ex57.py
+++
@@ -1,6 +0,0 @@
-#EXERCICIO 57
-
-g = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
-while g not in 'MmFf':
-   g = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
-print('Sexo {} registrado com sucesso'.format(g))
